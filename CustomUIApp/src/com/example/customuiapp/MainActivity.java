45d36f3dd582fc7ddc1333592aea63bbec581a4d
Eric Templin <ertemplin@gmail.com>
2013-10-01 19:34:16 -0400

    Implement simple feet to meters converter.

--- CustomUIApp/src/com/example/customuiapp/MainActivity.java
+++ CustomUIApp/src/com/example/customuiapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.customuiapp;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +13,16 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		findViewById(R.id.button1).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				float number = Float.parseFloat(((EditText)findViewById(R.id.editText1)).getText().toString());
+				number *= 0.3048;
+				((EditText)findViewById(R.id.editText2)).setText(Float.toString(number));
+			}
+			
+		});
 	}
 
 	@Override
